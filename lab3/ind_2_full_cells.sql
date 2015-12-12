6de7572133e4d00b5bf2b8f4f90734914cf7516b
FeDoR_MuRaVeV <muravevfedor@mail.ru>
2015-12-12 23:00:18 +0300

    report_3_changes
    Исправленный отчет по 3-ей лабораторной работе

--- lab3/ind_2_full_cells.sql
+++ lab3/ind_2_full_cells.sql
@@ -1,4 +1,2 @@
-create view ind_arend_cells as select repository.repository_id, count(cells.cell_number) as kol from cells,  repository
-where (cells.status='arrend')and(repository.repository_id=cells.repository_id)and(cells.cell_number in (select cell_number from history_cell
-where (acsess_type=1)and(item_id not in (select item_id from history_cell
-where acsess_type=2)))) group by repository.repository_id;
+create view ind_arend_cells as select repository.repository_id, count(contracts.contract_id) as kol from cells,  repository, contracts
+where (repository.repository_id=cells.repository_id)and(cells.cell_number=contracts.cell_number)and(contracts.end_date>current_date) group by repository.repository_id;
